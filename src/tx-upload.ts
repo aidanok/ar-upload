1f0786c913a6234466f40d26c9965fab5092b597
aidanok <therealaok@gmail.com>
2020-01-08 11:51:37 +0000

    add order field to TxUpload

--- src/tx-upload.ts
+++ src/tx-upload.ts
@@ -2,9 +2,11 @@ import { TransactionParameters } from "./transaction-parameters";
 
 export interface TxUpload {
   identifier: string;
+  order: number;
   id: null | string;
   confirmations: number;
   status: number;
   transaction: null | TransactionParameters;
   byteSize: number;
+
 }
--- src/upload.ts
+++ src/upload.ts
@@ -2,24 +2,23 @@ import { Options } from "./options";
 import { TxUpload } from "./tx-upload";
 
 /**
- * A serializable data structure that holds the current
- * state and options of an Upload. It has a couple of helper 
- * methods for construction and reading data, but does not contain 
+ * Serializable data structure that holds the current
+ * state and options of an Upload. It has a couple of helper
+ * methods for construction and reading data, but does not contain
  * any other logic.
- * 
- * It can be serialized by calling JSON.stringify() and de-serialized 
+ *
+ * It can be serialized by calling JSON.stringify() and de-serialized
  * by calling one of the static fromJSON methods.
- * 
+ *
  */
 export class Upload implements Options {
-
   maxPendingBytes: number = 40 * 1024 * 1024;
   maxPendingTxs: number = 40;
   pollTime: number = 20;
   confirmationsRequired: number = 4;
   MAX_TX_SIZE: number = 10 * 1024 * 1024;
- 
-  private all: TxUpload[] = []
+
+  private all: TxUpload[] = [];
 
   public constructor(identifiers: string[] = [], options: Partial<Options> = {}) {
     Object.assign(this, options);
@@ -29,19 +28,30 @@ export class Upload implements Options {
       confirmations: -1,
       status: 404,
       transaction: null,
-      byteSize: 0
+      byteSize: 0,
+      order: 0
     }));
   }
 
+  public addIdentifiers(identifiers: string[], order = 0) {
+    this.all.push(
+      ...identifiers.map(identifier => ({
+        identifier,
+        id: null,
+        confirmations: -1,
+        status: 404,
+        transaction: null,
+        byteSize: 0,
+        order: 1
+      }))
+    );
+  }
+
   // When serializing, we don't want to store the transaction
-  // data which could be very large, so just make a copy and 
-  // overwrite transaction with null. 
+  // data which could be very large, so just make a copy and
+  // overwrite transaction with null.
   public toJSON(): string {
-    const copy = Object.assign(
-      {},
-      this, 
-      { all: this.all.map(x => Object.assign(x, { transaction: null })) }, 
-    )
+    const copy = Object.assign({}, this, { all: this.all.map(x => Object.assign(x, { transaction: null })) });
     return JSON.stringify(copy);
   }
 
@@ -73,28 +83,26 @@ export class Upload implements Options {
   }
 
   public get queuedBytes(): number {
-    return this.queued.reduce((total, x) => total += x.byteSize, 0);
+    return this.queued.reduce((total, x) => (total += x.byteSize), 0);
   }
 
   public get pendingBytes(): number {
-    return this.pending.reduce((total, x) => total += x.byteSize, 0);
+    return this.pending.reduce((total, x) => (total += x.byteSize), 0);
   }
 
   public get minedBytes(): number {
-    return this.mined.reduce((total, x) => total += x.byteSize, 0);
+    return this.mined.reduce((total, x) => (total += x.byteSize), 0);
   }
 
   public get completeBytes(): number {
-    return this.complete.reduce((total, x) => total += x.byteSize, 0);
+    return this.complete.reduce((total, x) => (total += x.byteSize), 0);
   }
-
 }
 
-export const isQueued = (x: TxUpload) => x.status === 404
+export const isQueued = (x: TxUpload) => x.status === 404;
 
-export const isPending = (x: TxUpload) => x.status === 202
+export const isPending = (x: TxUpload) => x.status === 202;
 
-export const isMined = (x: TxUpload, confirmationsRequired: number) => x.status === 200 && x.confirmations < confirmationsRequired
+export const isMined = (x: TxUpload, confirmationsRequired: number) => x.status === 200 && x.confirmations < confirmationsRequired;
 
-export const isComplete = (x: TxUpload, confirmationsRequired: number) => x.status === 200 && x.confirmations >= confirmationsRequired
-
+export const isComplete = (x: TxUpload, confirmationsRequired: number) => x.status === 200 && x.confirmations >= confirmationsRequired;
